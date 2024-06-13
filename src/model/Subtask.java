829f44a2dba6a3c511fa26f64728048460d1e3ec
Sergej Kostarev <kostarevsergo@yandex.ru>
2024-06-13 22:41:42 +0300

    Fix codestyle #7

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -3,7 +3,7 @@ package model;
 public class Subtask extends Task {
 
     private final Integer epicId;
-    
+
     public Subtask(String name, String description, TaskStatus status, Integer epicId) {
         super(name, description, status);
         this.epicId = epicId;
