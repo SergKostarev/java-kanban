ac3110478302a6e95d91a64cf17fb426c40bbada
Sergej Kostarev <kostarevsergo@yandex.ru>
2024-07-28 15:15:32 +0300

    Fix codestyle

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
-import model.Epic;
-import model.Subtask;
-import model.Task;
-import model.TaskStatus;
-import service.Managers;
-import service.TaskManager;
-
-import java.time.Duration;
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
